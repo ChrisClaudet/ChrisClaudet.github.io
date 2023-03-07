48a40853bc14cec4b048c2b99daac40b3770fb46
Christopher Claudet <christopherclaudet@ChristophersMBP.fritz.box>
2023-03-07 19:39:49 +0000

    DOM selector section 24 added

---
+++ Section 24/querySelector.js
@@ -0,0 +1,9 @@
+//finds first h1 element:
+document.querySelector('h1');
+
+//finds first element with ID of red
+document.querySelector('#red');
+
+//finds first element with class of big
+document.querySelector('.big');
+
